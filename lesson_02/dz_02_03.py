0c2d6dcbddfe3bdabe078ad044b97daa030eb0cf
Sonya Y <yasenkovasofia@gmail.com>
2021-06-25 15:39:51 +0300

    lesson_02_final

--- lesson_02/dz_02_03.py
+++ lesson_02/dz_02_03.py
@@ -1,6 +1,6 @@
 input_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-i = 0
 
+i = 0
 while i < len(input_list):
     if (input_list[i].isdigit() == True) or ('+' in input_list[i]):
         input_list.insert(i, '"')
@@ -15,12 +15,9 @@ while i < len(input_list):
         i += 1
 
 string_to_print = ' '.join(input_list)
-print(string_to_print)
-result = ''
-j = 0
 i = 0
 index_list = []
-while i < len(string_to_print)+1:
+while i < len(string_to_print) + 1:
     index_01 = string_to_print.find('"', i)
     if index_01 == -1:
         index_list.append(len(string_to_print))
@@ -30,7 +27,7 @@ while i < len(string_to_print)+1:
     index_list.append(index_01)
 
 new_string = ''
-for i in range(0, len(index_list)-1):
+for i in range(0, len(index_list) - 1):
     if i % 2 == 0:
         new_string += string_to_print[index_list[i]:index_list[i + 1]].replace(' ', '')
     else:
--- lesson_02/dz_05.py
+++ lesson_02/dz_05.py
@@ -6,13 +6,9 @@ string_for_print = ''
 print('Задание А. Список цен по формату')
 for i, item in enumerate(input_list):
     rub = int(item // 1)
-    kop = int((item % 1) * 101)  # проверить выражение, некоторые числа меньше на 1
-    input_list[i] = f'{rub} руб {kop:02d} коп'
+    penny = int((item % 1) * 101)  # проверить выражение, некоторые числа меньше на 1
+    input_list[i] = f'{rub} руб {penny:02d} коп'
 print(", ".join(input_list))
-
-
-#     string_for_print += f'{rub} руб {kop:02d} коп, '
-# print(string_for_print.strip(', '))
 
 print('\nЗадание В. Сортировка в порядке возрастания')
 
@@ -20,15 +16,14 @@ input_list.sort()
 print(", ".join(input_list))
 sorted_id = id(input_list)
 
-#доказательство идентичных списков
+# доказательство идентичных списков
 if unsorted_id == sorted_id:
     print(f'\nID сортированного и несортироавнного списка равны = {sorted_id}. Значит, объект остался тот же.')
 
-#Задание C. Без вывода инфы
+# Задание C. Без вывода инфы
 
 string_for_print = ''
 reversed_list = sorted(input_list, reverse=True)
 
-#Задание D. 5 самых дорогих товара. По возможности, минимум кода
+# Задание D. 5 самых дорогих товара. По возможности, минимум кода
 print('\nЗадание D. 5 самых дорогих товаров:', ", ".join(reversed_list[:5]))
-
